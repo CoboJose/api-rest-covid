d448fa15396ffdda6564e0b699f3d75bf63ebeea
Jose Cobo <cobogue@gmail.com>
2021-05-17 03:34:36 +0200

    Back

---
+++ server/src/main/java/cbd52/server/models/TopNewConfirmedProvince.java
@@ -0,0 +1,10 @@
+package cbd52.server.models;
+
+import lombok.Data;
+
+@Data
+public class TopNewConfirmedProvince {
+	String name;
+	Integer newConfirmed;
+	
+}
--- server/src/main/java/cbd52/server/repositories/DateDataRepository.java
+++ server/src/main/java/cbd52/server/repositories/DateDataRepository.java
@@ -1,7 +1,10 @@
 package cbd52.server.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cbd52.server.models.DateData;
@@ -11,4 +14,7 @@ public interface DateDataRepository extends MongoRepository<DateData, String>{
     
 	@Aggregation(pipeline = { "{$sort:{timestamp:-1}}", "{$limit:1}", "{$project: {_id:1}}" })
 	public String findLastDay();
+	
+	@Query("{timestamp: {$gte:?0, $lte:?1}}")
+	public List<DateData> getByRange(int from, int to);
 }
